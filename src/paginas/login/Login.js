e91cc703afda1adb609211fc1417af264a7ee949
marcuxo <marcourrutiam@gmail.com>
2023-02-03 17:34:12 -0300

    litle changes

---
+++ src/paginas/login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <div>
+            <img src='/img/logo_ariztia.png' />
+          </div>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
